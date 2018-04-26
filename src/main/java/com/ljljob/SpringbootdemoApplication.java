baa0c32d4f1dd7cac408890fef3606c97fa463d2
wujianmin <you@example.com>
2018-04-26 16:21:24 +0800

    init

---
+++ src/main/java/com/ljljob/SpringbootdemoApplication.java
@@ -0,0 +1,14 @@
+package com.ljljob;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = {"com.ljljob.mapper"})
+public class SpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ljljob/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.ljljob.controller;
+
+import com.ljljob.entity.User;
+import com.ljljob.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/list")
+    public List<User> list() {
+        return userMapper.selectByExample(null);
+    }
+}
